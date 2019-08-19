13b26c7d0d513427cb482e4dc261e99c26855d87
Zachary Reece <zachdreece@gmail.com>
2019-08-19 18:41:59 -0500

    Finished migration to Next.js

--- src/Link.js
+++ src/Link.js
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withRouter } from 'next/router';
-import NextLink from 'next/link';
-import MuiLink from '@material-ui/core/Link';
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { withRouter } from "next/router";
+import NextLink from "next/link";
+import MuiLink from "@material-ui/core/Link";
 
 const NextComposed = React.forwardRef(function NextComposed(props, ref) {
   const { as, href, prefetch, ...other } = props;
@@ -19,14 +19,14 @@ const NextComposed = React.forwardRef(function NextComposed(props, ref) {
 NextComposed.propTypes = {
   as: PropTypes.string,
   href: PropTypes.string,
-  prefetch: PropTypes.bool,
+  prefetch: PropTypes.bool
 };
 
 // A styled version of the Next.js Link component:
 // https://nextjs.org/docs/#with-link
 function Link(props) {
   const {
-    activeClassName = 'active',
+    activeClassName = "active",
     router,
     className: classNameProps,
     innerRef,
@@ -35,14 +35,21 @@ function Link(props) {
   } = props;
 
   const className = clsx(classNameProps, {
-    [activeClassName]: router.pathname === props.href && activeClassName,
+    [activeClassName]: router.pathname === props.href && activeClassName
   });
 
   if (naked) {
     return <NextComposed className={className} ref={innerRef} {...other} />;
   }
 
-  return <MuiLink component={NextComposed} className={className} ref={innerRef} {...other} />;
+  return (
+    <MuiLink
+      component={NextComposed}
+      className={className}
+      ref={innerRef}
+      {...other}
+    />
+  );
 }
 
 Link.propTypes = {
@@ -55,10 +62,12 @@ Link.propTypes = {
   onClick: PropTypes.func,
   prefetch: PropTypes.bool,
   router: PropTypes.shape({
-    pathname: PropTypes.string.isRequired,
-  }).isRequired,
+    pathname: PropTypes.string.isRequired
+  }).isRequired
 };
 
 const RouterLink = withRouter(Link);
 
-export default React.forwardRef((props, ref) => <RouterLink {...props} innerRef={ref} />);
+export default React.forwardRef((props, ref) => (
+  <RouterLink {...props} innerRef={ref} />
+));
