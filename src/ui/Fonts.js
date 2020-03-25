742ce6adec204c3cdf712cab0d619e602a6cd72e
Zachary Reece <zacharyreece@Zacharys-iMac.local>
2020-03-25 13:07:40 -0500

    Testing non-blocking fonts solution

---
+++ src/ui/Fonts.js
@@ -0,0 +1,30 @@
+import FontFaceObserver from "fontfaceobserver";
+
+const Fonts = () => {
+  const link = document.createElement("link");
+  link.href =
+    "https://fonts.googleapis.com/css?family=Pacifico|Raleway:100,400,400i,700|Roboto:300,400,500,700&display=swap";
+  link.rel = "stylesheet";
+
+  document.head.appendChild(link);
+
+  const roboto = new FontFaceObserver("Roboto");
+
+  roboto.load().then(() => {
+    document.documentElement.classList.add("roboto");
+  });
+
+  const raleway = new FontFaceObserver("Raleway");
+
+  raleway.load().then(() => {
+    document.documentElement.classList.add("raleway");
+  });
+
+  const pacifico = new FontFaceObserver("Pacifico");
+
+  pacifico.load().then(() => {
+    document.documentElement.classList.add("pacifico");
+  });
+};
+
+export default Fonts;
