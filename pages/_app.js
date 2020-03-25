495ed8f51474d0f7709d947a38ab97ebb0199135
Zachary Reece <zachdreece@gmail.com>
2020-03-25 13:19:24 -0500

    Testing new git settings

--- pages/_app.js
+++ pages/_app.js
@@ -27,6 +27,7 @@ export default class MyApp extends App {
 
   componentDidMount() {
     Fonts();
+
     // Check that service workers are supported
     if ("serviceWorker" in navigator) {
       // Use the window load event to keep the page load performant
